00964abf5790e7ceafd11bc8d7c6c4f27a67f80c
hakeem compaore <thecompaoreboy@gmail.com>
2017-07-03 17:08:16 +0000

    contact_form

--- db/migrate/20170629165551_create_contac.rb
+++ db/migrate/20170629165551_create_contac.rb
@@ -1,6 +1,10 @@
 class CreateContac < ActiveRecord::Migration
   def change
     create_table :contacs do |t|
+      t.string :name
+      t.string :email
+      t.strings  :comments 
+      t.timestampds
     end
   end
 end
